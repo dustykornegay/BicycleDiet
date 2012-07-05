2aa259cded14e11fe4f4acff6e38f1c4f63697a8
Dusty <dusty@ieee.org>
2012-07-04 23:28:28 -0700

    Split up Database file check and sql execution

--- BicycleDiet/DBController.m
+++ BicycleDiet/DBController.m
@@ -50,10 +50,8 @@
     return self;
 }
 
-
-
--(BOOL)  DBdatafieldToObjectArray: (NSString *) sql_com{
-	// Execute the "checkAndCreateDatabase" function
+-(BOOL) CheckOrCreateDB {
+    // Execute the "checkAndCreateDatabase" function
 	// Check if the SQL database has already been saved to the users phone, if not then copy it over
 	BOOL success;
 	
@@ -63,19 +61,28 @@
 	
 	// Check if the database has already been created in the users filesystem
 	success = [fileManager fileExistsAtPath:databasePath];
-	
     
 	// If the database already exists then return without doing anything
 	if(!success) {
+        
+        // If not then proceed to copy the database from the application to the users filesystem
+        
+        // Get the path to the database in the application package
+        NSString *databasePathFromApp =[[[NSBundle mainBundle]resourcePath] stringByAppendingPathComponent:databaseName];
+        
+        // Copy the database from the package to the users filesystem
+       success = [fileManager copyItemAtPath:databasePathFromApp toPath:databasePath error:nil];
+        
+        }
+    
+    return success;
+    
+    
+}
+
+-(BOOL)  DBdatafieldToObjectArray: (NSString *) sql_com{
 	
-	// If not then proceed to copy the database from the application to the users filesystem
-	
-	// Get the path to the database in the application package
-	NSString *databasePathFromApp =[[[NSBundle mainBundle]resourcePath] stringByAppendingPathComponent:databaseName];
-	
-	// Copy the database from the package to the users filesystem
-	[fileManager copyItemAtPath:databasePathFromApp toPath:databasePath error:nil];
-	} else {
+	BOOL success = [self CheckOrCreateDB];
         
 	// Query the database for all  records and construct the object array
         
@@ -123,7 +130,7 @@
 		// Release the compiled statement from memory
 		sqlite3_finalize(compiledStatement);
     }
-        }
+
 
 sqlite3_close(database);
     return success;
